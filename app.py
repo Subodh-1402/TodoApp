6a628527efd6024eedac3ed6d7b074da4d2e0067
Subodh Jage <jagesubodh5259@gmail.com>
2023-06-06 16:56:40 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    db = get_database()
+    db = connect_to_database()#get_database()
     task_cursor = db.execute("select * from todolist")
     alltasks = task_cursor.fetchall()
     return render_template("index.html", alltasks = alltasks)
@@ -16,7 +16,7 @@ def inserttask():
     if request.method == "POST":
         # get the task entered by the user from the form.
         enteredtask = request.form['todaystask']
-        db = get_database()
+        db = connect_to_database()#get_database()
         db.execute("insert into todolist (task) values(?)", [enteredtask])
         db.commit()
         return redirect(url_for("index"))
@@ -26,7 +26,7 @@ def inserttask():
 @app.route("/deletetask/<int:id>", methods = ["POST", "GET"])
 def deletetask(id):
     if request.method == "GET":
-        db = get_database()
+        db = connect_to_database()#get_database()
         db.execute("delete from todolist where id = ?", [id])
         db.commit()
         return redirect(url_for("index"))
